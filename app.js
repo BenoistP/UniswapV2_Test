52f441dd1122a3091e4e5248ab36dd75361dfc7f
BenoistP <bp_github.com@gazouille.net>
2023-06-22 13:14:27 +0200

    tmp

--- app.js
+++ app.js
@@ -9,88 +9,6 @@ const { getChainName,getChainId,getRpcUrl,getDaiTokenAddress } = require('./tool
 
 
 
-/*
-const CHAINID_ETH_MAINNET = ChainId.MAINNET;
-const CHAINID_ETH_TEST_GOERLI = ChainId.GÖRLI;
-const CHAINID_ETH_TEST_SEPOLIA = 11155111; // 11155111(0xaa36a7)
-const CHAINID_POLYGON_TEST_MUMBAI = 80001; // 80001(0x13881)
-
-
-const daitokenAddress_MAINNET = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // DAI address mainnet
-const daitokenAddress_ETH_TEST_SEPOLIA = '0x3e622317f8C93f7328350cF0B56d9eD4C620C5d6'; // DAI address sepolia
-const daitokenAddress_ETH_TEST_GOERLI = '0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844'; // DAI address goerli
-const daitokenAddress_POLYGON_TEST_MUMBAI = '0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F'; // DAI address mumbai
-
-const CHAIN_ETHEREUM_MAINNET = "ETHEREUM_MAINNET"
-const CHAIN_ETHEREUM_TESTNET_SEPOLIA = "ETHEREUM_TESTNET_SEPOLIA"
-const CHAIN_ETHEREUM_TESTNET_GOERLI = "ETHEREUM_TESTNET_GOERLI"
-const CHAIN_POLYGON_TESTNET_POLYGON_MUMBAI = "POLYGON_TESTNET_MUMBAI"
-*/
-/* 
-const CHAIN_NAME = process.env.CHAIN_NAME
-console.debug(`CHAIN_NAME = "${CHAIN_NAME}"`)
-
-const CHAIN_ETHEREUM_MAINNET_RPC = process.env.RPC_ETHEREUM_MAINNET
-console.debug(`CHAIN_ETHEREUM_MAINNET_RPC = "${CHAIN_ETHEREUM_MAINNET_RPC}"`)
-const CHAIN_ETHEREUM_TESTNET_SEPOLIA_RPC = process.env.RPC_ETHEREUM_TESTNET_SEPOLIA
-console.debug(`CHAIN_ETHEREUM_TESTNET_SEPOLIA_RPC = "${CHAIN_ETHEREUM_TESTNET_SEPOLIA_RPC}"`)
-const CHAIN_ETHEREUM_TESTNET_GOERLI_RPC = process.env.RPC_ETHEREUM_TESTNET_GOERLI
-console.debug(`CHAIN_ETHEREUM_TESTNET_GOERLI_RPC = "${CHAIN_ETHEREUM_TESTNET_GOERLI_RPC}"`)
-const CHAIN_POLYGON_TESTNET_POLYGON_MUMBAI_RPC = process.env.RPC_POLYGON_TESTNET_POLYGON_MUMBAI
-console.debug(`CHAIN_POLYGON_TESTNET_POLYGON_MUMBAI_RPC = "${CHAIN_POLYGON_TESTNET_POLYGON_MUMBAI_RPC}"`)
-
-const CHAIN_ID = getChainId(CHAIN_NAME)
-console.debug(`CHAIN_ID = "${CHAIN_ID}"`)
- */
-/*
-const getChainId = (chainName) => {
-  switch (chainName) {
-    case CHAIN_ETHEREUM_MAINNET:
-      return CHAINID_ETH_MAINNET
-    case CHAIN_ETHEREUM_TESTNET_SEPOLIA:
-      return CHAINID_SEPOLIA
-    case CHAIN_ETHEREUM_TESTNET_GOERLI:
-      return CHAINID_ETH_TEST_GOERLI
-    case CHAIN_POLYGON_TESTNET_POLYGON_MUMBAI:
-      return CHAINID_POLYGON_TEST_MUMBAI
-    default:
-      return -1
-  }
-}
-
-const getRpcUrl = (chainName) => {
-  switch (chainName) {
-    case CHAIN_ETHEREUM_MAINNET:
-      return CHAIN_ETHEREUM_MAINNET_RPC
-    case CHAIN_ETHEREUM_TESTNET_SEPOLIA:
-      return CHAIN_ETHEREUM_TESTNET_SEPOLIA_RPC
-    case CHAIN_ETHEREUM_TESTNET_GOERLI:
-      return CHAIN_ETHEREUM_TESTNET_GOERLI_RPC
-    case CHAIN_POLYGON_TESTNET_POLYGON_MUMBAI:
-      return CHAIN_POLYGON_TESTNET_POLYGON_MUMBAI_RPC
-    default:
-      return ""
-  }
-}
-
-const getDaiTokenAddress = (chainName) => {
-  switch (chainName) {
-    case CHAIN_ETHEREUM_MAINNET:
-      return daitokenAddress_MAINNET
-    case CHAIN_ETHEREUM_TESTNET_SEPOLIA:
-      return daitokenAddress_ETH_TEST_SEPOLIA
-    case CHAIN_ETHEREUM_TESTNET_GOERLI:
-      return daitokenAddress_ETH_TEST_GOERLI
-    case CHAIN_POLYGON_TESTNET_POLYGON_MUMBAI:
-      return daitokenAddress_POLYGON_TEST_MUMBAI
-    default:
-      return -1
-  }
-}
-*/
-
-
-
 const init = async () => {
 
 
@@ -128,6 +46,10 @@ console.debug(`dai =`)
 console.dir(dai)
 
 
+process.exit(0)
+
+// ----------------
+
  const pair = await Fetcher.fetchPairData(dai, weth);
  const route = new Route([pair], weth);
  const trade = new Trade(route, new TokenAmount(weth, '100000000000000000'), TradeType.EXACT_INPUT);
@@ -136,10 +58,6 @@ console.dir(dai)
  console.log(trade.executionPrice.toSignificant(6));
  console.log(trade.nextMidPrice.toSignificant(6));
  
-
-process.exit(0)
-
-// ----------------
 
 const slippageTolerance = new Percent('50', '10000'); // tolérance prix 50 bips = 0.050%
  
